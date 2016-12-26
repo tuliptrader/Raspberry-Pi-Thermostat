9b0f07e4b08d1d8c27c75cfb38305c36aac59fda
Mathieu Langeslag <renelangeslag@Mathieu-Macbook.local>
2016-12-26 15:12:48 +0100

    Fix Temperature_Logger3.py to not concatenate string and an integer. the temperature integer is converted into a string before being concatenated with the datetime Attempt #2

--- Temperature_Logger3.py
+++ Temperature_Logger3.py
@@ -12,8 +12,9 @@ log_interval = 5
 while True:
     datafile = open("temperature_data_py.csv", "a")
     thermocouple = MAX6675(cs_pin, clock_pin, data_pin, units)
+    temperature_now=thermocouple.get()
 
-    info_now =str(datetime.now()+"; "+str(thermocouple.get()))
+    info_now =str( datetime.now()+"; "+str(temperature_now))
     datafile.write(info_now)
 
     print(info_now)
