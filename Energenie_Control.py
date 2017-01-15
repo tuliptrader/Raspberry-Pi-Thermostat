e2ea5c6e0952297e287ab3331f6bb87645c8520a
Mathieu L <mrlangeslag@gmail.com>
2017-01-15 17:43:35 +0000

    File to test energenie file Update #2

--- Energenie_Control.py
+++ Energenie_Control.py
@@ -1,10 +1,40 @@
 # Code to turn Energenie sockets on and off
 
+#import the required modules
+import RPi.GPIO as GPIO
+import time
 
+# set the pins numbering mode
+GPIO.setmode(GPIO.BOARD)
+
+# Select the GPIO pins used for the encoder K0-K3 data inputs
+GPIO.setup(11, GPIO.OUT)
+GPIO.setup(15, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+
+# Select the signal to select ASK/FSK
+GPIO.setup(18, GPIO.OUT)
+
+# Select the signal used to enable/disable the modulator
+GPIO.setup(22, GPIO.OUT)
+
+# Disable the modulator by setting CE pin lo
+GPIO.output (22, False)
+
+# Set the modulator to ASK for On Off Keying
+# by setting MODSEL pin lo
+GPIO.output (18, False)
+
+# Initialise K0-K3 inputs of the encoder to 0000
+GPIO.output (11, False)
+GPIO.output (15, False)
+GPIO.output (16, False)
+GPIO.output (13, False)
 
 def energenie_socket(socket, state): # sockets (1,2,3,4,all) and state (True, False)
 	# We will just loop round switching the units on and off
-	if socket == 1:
+    if socket == 1:
         if state == True:
             # Set K0-K3
             #print "sending code 1111 socket 1 on"
@@ -21,7 +51,7 @@ def energenie_socket(socket, state): # sockets (1,2,3,4,all) and state (True, Fa
             # Disable the modulator
             GPIO.output (22, False)
 
-        if state == False:
+        elif state == False:
             # Set K0-K3
             #print "sending code 0111 Socket 1 off"
             GPIO.output (11, True)
@@ -37,7 +67,7 @@ def energenie_socket(socket, state): # sockets (1,2,3,4,all) and state (True, Fa
             # Disable the modulator
             GPIO.output (22, False)
 
-    if socket == 2:
+    elif socket == 2:
         if state == True:
             # Set K0-K3
             #print "sending code 1110 socket 2 on"
@@ -54,7 +84,7 @@ def energenie_socket(socket, state): # sockets (1,2,3,4,all) and state (True, Fa
             # Disable the modulator
             GPIO.output (22, False)
 
-        if State == False
+        elif state == False:
             # Set K0-K3
             #print "sending code 0110 socket 2 off"
             GPIO.output (11, False)
@@ -69,7 +99,7 @@ def energenie_socket(socket, state): # sockets (1,2,3,4,all) and state (True, Fa
             time.sleep(0.25)
             # Disable the modulator
             GPIO.output (22, False)
-    if socket == 3:
+    elif socket == 3:
 		if state == True:
             # Set K0-K3
             #print "sending code 1101 socket 3 on"
@@ -86,7 +116,7 @@ def energenie_socket(socket, state): # sockets (1,2,3,4,all) and state (True, Fa
             # Disable the modulator
             GPIO.output (22, False)
 
-		if state == False:
+		elif state == False:
             # Set K0-K3
             #print "sending code 0101 socket 3 off"
             GPIO.output (11, True)
@@ -102,7 +132,7 @@ def energenie_socket(socket, state): # sockets (1,2,3,4,all) and state (True, Fa
             # Disable the modulator
             GPIO.output (22, False)
 
-	if socket == 4:
+	elif socket == 4:
         if state == True:
             # Set K0-K3
             #print "sending code 1100 socket 4 on"
@@ -119,7 +149,7 @@ def energenie_socket(socket, state): # sockets (1,2,3,4,all) and state (True, Fa
             # Disable the modulator
             GPIO.output (22, False)
 
-		if state == False:
+		elif state == False:
             # Set K0-K3
             print "sending code 0100 socket 4 off"
             GPIO.output (11, False)
@@ -135,8 +165,8 @@ def energenie_socket(socket, state): # sockets (1,2,3,4,all) and state (True, Fa
             # Disable the modulator
             GPIO.output (22, False)
 
-    if socket == all:
-        if state == True:
+    elif socket == all:
+         if state == True:
 		# Set K0-K3
             #print "sending code 1011 ALL on"
             GPIO.output (11, True)
@@ -152,7 +182,7 @@ def energenie_socket(socket, state): # sockets (1,2,3,4,all) and state (True, Fa
             # Disable the modulator
             GPIO.output (22, False)
 
-        if state == False:
+         elif state == False:
             # Set K0-K3
             #print "sending code 0011 All off"
             GPIO.output (11, True)
