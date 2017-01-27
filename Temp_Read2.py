708a2bcb305f70905b5d2f582a13c7222c741d49
Mathieu Langeslag <mrlangeslag@gmail.com>
2017-01-27 23:41:29 +0000

    File to output temperature from DS18B20 thermometer. Fixed w1thermsensor folder to remove "-folder" suffix #3

--- Temp_Read2.py
+++ Temp_Read2.py
@@ -1,7 +1,8 @@
 #Read the temperature from a DS18B20 Thermometer
 
-from w1thermsensor import W1ThermSensor
-
+from w1thermsensor.w1thermsensor import W1ThermSensor
 
 sensor = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "0416606a3cff")
 temperature_in_celsius = sensor.get_temperature()
+
+print temperature_in_celsius
