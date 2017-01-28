c32be4106f1c86aea6a1ae00b3439e9dddda7228
Mathieu Langeslag <mrlangeslag@gmail.com>
2017-01-28 23:21:36 +0000

    File that returns a ten minute average temperature of the last 10 minutes #3

--- Ten_minute_temp.py
+++ Ten_minute_temp.py
@@ -10,17 +10,17 @@
 
 def Ten_Minute_Average2():
     file_x = open('/home/pi/Python_Stuff/Thermostat_Data/Data.csv')
-    print(file_x)
+    #print(file_x)
     last_ten_lines = list(file_x)[-10:]
-    last_ten_one = last_ten_lines[1]
+    #last_ten_one = last_ten_lines[1]
 
-    print(last_ten_lines)
-    print(last_ten_one)
-    print(last_ten_one[28:])
+    #print(last_ten_lines)
+    #print(last_ten_one)
+    #print(last_ten_one[28:])
 
     total = float(0)
     for x in range(0,10,1):
-        print x
+        #print x
         k = last_ten_lines[x]
         variable = k[28:]
         total = total + float(variable)
