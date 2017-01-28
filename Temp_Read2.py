ca78efe175446f4bb2d49462162931c343952710
Mathieu Langeslag <mrlangeslag@gmail.com>
2017-01-28 16:30:18 +0000

    File to output temperature from DS18B20 thermometer. Fixed w1thermsensor folder to remove "-folder" suffix #17

--- Temp_Read2.py
+++ Temp_Read2.py
@@ -1,11 +1,20 @@
 #Read the temperature from a DS18B20 Thermometer
 
-with open('/sys/bus/w1/devices/28-0416606a3cff/w1_slave', 'r') as content_file:
-    Temperature_Data = content_file.read()
-    print Temperature_Data
+#with open('/sys/bus/w1/devices/28-0416606a3cff/w1_slave', 'r') as content_file:
+#    Temperature_Data = content_file.read()
+ #   print Temperature_Data
+#
+ #   variable = Temperature_Data[-6:]
+#
+ #   Living_Room = float(variable) / 1000
+#
+ #   print Living_Room
 
-    variable = Temperature_Data[-6:]
+open('/sys/bus/w1/devices/28-0416606a3cff/w1_slave', 'r') as content_file:
 
-    Living_Room = float(variable) / 1000
+Temperature_Data = content_file.read()
+print   Temperature_Data
+variablex =Temperature_Data[-6:]
 
-    print Living_Room
+Living_room = float(variablex)/1000
+return Living_room
