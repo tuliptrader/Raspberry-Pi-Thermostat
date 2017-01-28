80b73152dacbb256a7f3a6cd1f9a8b33609ff041
Mathieu Langeslag <mrlangeslag@gmail.com>
2017-01-28 17:12:25 +0000

    Code to write temperature to a log on the Pi #4
    writes temperature and time information to outside folder

--- Temp_Read2.py
+++ Temp_Read2.py
@@ -3,17 +3,18 @@ from datetime import datetime
 
 with open('/sys/bus/w1/devices/28-0416606a3cff/w1_slave', 'r') as content_file:
     Temperature_Data = content_file.read()
-    print(Temperature_Data)
+    #print(Temperature_Data)
 
     variable = Temperature_Data[-6:]
 
     Living_Room = float(variable) / 1000
 
-    print(str(Living_Room) + "Celsius")
+    #print(str(Living_Room) + "Celsius")
 
     datafile = open("/home/pi/Python_Stuff/Thermostat_Data/Data.csv", "a")
 
     info_now = str(datetime.now()) + "; " + str(Living_Room) + "\n"
+
     datafile.write(info_now)
 
     print(info_now)
