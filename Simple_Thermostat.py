935a91ccf193dafa8744e4e34140a6c3df46b7cf
Mathieu Langeslag <mrlangeslag@gmail.com>
2017-01-31 20:52:05 +0000

    Test file for Energenie Control
    Simple Thermostat
    
    Energenie_Control - commented out a lot of the code that had line errors
    
    #9
    
    changed to 16Celsius

--- Simple_Thermostat.py
+++ Simple_Thermostat.py
@@ -2,10 +2,10 @@ from Ten_minute_temp import Ten_Minute_Average2
 from Energenie_Control import energenie_socket
 
 def Heat_or_no_heat():
-    if Ten_Minute_Average2() < 15: #Turn on socket
+    if Ten_Minute_Average2() < 16: #Turn on socket
         energenie_socket(1,True)
 
-    elif Ten_Minute_Average2() <= 15: #Turn off socket
+    elif Ten_Minute_Average2() <= 16: #Turn off socket
         energenie_socket(1,False)
 
 
