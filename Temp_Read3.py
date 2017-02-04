5f6e3dd7e7b4ea3b73f644c230abb86f87fa57e9
Mathieu Langeslag <mrlangeslag@gmail.com>
2017-02-04 21:55:55 +0000

    Temp_Read3 file uploaded
    Test with regular expressions #1

---
+++ Temp_Read3.py
@@ -0,0 +1,20 @@
+#Read the temperature from a DS18B20 Thermometer
+from datetime import datetime
+import re
+
+content_file = open('/sys/bus/w1/devices/28-0416606a3cff/w1_slave', 'r')
+
+Temperature_Data = re.findall("[t=].....", content_file)
+
+Living_Room = float(Temperature_Data) / 1000
+
+print(str(Living_Room) + "Celsius")
+
+datafile = open("/home/pi/Python_Stuff/Thermostat_Data/Data3.csv", "a")
+
+info_now = str(datetime.now()) + "; " + str(Living_Room) + "\n"
+
+datafile.write(info_now)
+
+print(info_now)
+datafile.close()
