3d60028c9fe78dc5381f57b593067d3cdd995c2d
Mathieu Langeslag <mrlangeslag@gmail.com>
2017-02-10 23:42:45 +0000

    Temp_Read3 file uploaded
    Converted content_file to a string
    
    Test with regular expressions #9

--- Temp_Read3.py
+++ Temp_Read3.py
@@ -2,9 +2,11 @@
 from datetime import datetime
 import re
 
-content_file = str(open('/sys/bus/w1/devices/28-0416606a3cff/w1_slave', 'r'))
+content_file = open('/sys/bus/w1/devices/28-0416606a3cff/w1_slave', 'r')
 
-print content_file
+x = str(content_file)
+
+print x
 
 Temperature_Data = re.search('t=(.+?)', content_file)
 
