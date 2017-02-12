42407260f38ec912fed072456f01dd0ca1023491
Mathieu Langeslag <mrlangeslag@gmail.com>
2017-02-12 17:13:39 +0000

    Temp_Read3 file uploaded
    Converted content_file to a string
    
    Test with regular expressions #30

--- Temp_Read3.py
+++ Temp_Read3.py
@@ -3,9 +3,9 @@ from datetime import datetime
 import re
 
 for line in open('/sys/bus/w1/devices/28-0416606a3cff/w1_slave', 'r'):
-	Temperature_Data = re.findall("t=\d{5}", line)
+	Temperature_Data = re.findall("\d{5}", line)
 
-Living_Room = float(str(Temperature_Data)[4:8])/1000
+Living_Room = float(str(Temperature_Data))/1000
 print Living_Room
 
 """print content_file.read()
@@ -24,7 +24,7 @@ print Temperature_Data
 
 #Living_Room = float(Temperature_Data) / 1000
 
-print(str(Living_Room) + "Celsius using RegEx")
+print(str(Living_Room) + " Celsius using RegEx")
 
 datafile = open("/home/pi/Python_Stuff/Thermostat_Data/Data3.csv", "a")
 
