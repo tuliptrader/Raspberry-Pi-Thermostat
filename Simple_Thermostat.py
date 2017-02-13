9ac62a9fd4e078053c8f4ebe3672a5016645ec0d
Mathieu Langeslag <mrlangeslag@gmail.com>
2017-02-13 21:20:28 +0000

    Temperature_schedule return file added

--- Simple_Thermostat.py
+++ Simple_Thermostat.py
@@ -1,11 +1,12 @@
 from Ten_minute_temp import Ten_Minute_Average2
 from Energenie_Control import energenie_socket
+from Temperature_schedule import Target_temperature
 
 def Heat_or_no_heat():
-    if Ten_Minute_Average2() < 18: #Turn on socket
+    if Ten_Minute_Average2() < Target_temperature(): #Turn on socket
         energenie_socket(1,True)
 
-    elif Ten_Minute_Average2() >= 18: #Turn off socket
+    elif Ten_Minute_Average2() >= Target_temperature(): #Turn off socket
         energenie_socket(1,False)
 
 
---
+++ Temperature_schedule.py
@@ -0,0 +1,28 @@
+from  datetime import datetime
+import calendar
+
+def Target_temperature():
+	my_date = datetime.now() # assign the date object to the variable my_date
+	The_weekday = calendar.day_name[my_date.weekday()] #find the day of the week object, and find out what this is as a written day
+	#print Weekday
+
+	The_hour = my_date.hour
+	The_minute = my_date.minute
+	#print my_date.hour
+	#print my_date.minute
+
+	my_str =  str(The_weekday) + ',' + str(The_hour) +':' + str(The_minute)[0]
+	#my_str = "Monday,02:1"
+	#print my_str
+
+	#my_regex = the string that I will be searching for in the Schedule.csv file
+
+	for line in open('Schedule_livingroom.csv','r'):
+		if my_str in line:
+			Info = line
+
+	#print Info
+
+	Target = Info[-4:-2]
+
+	return Target
