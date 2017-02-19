b0ccb2271b6f3755fb894f313676204637e9ee4d
Mathieu Langeslag <mrlangeslag@gmail.com>
2017-02-19 17:57:05 +0000

    Added cleanup to Simple_Thermostat.py #1

--- Simple_Thermostat.py
+++ Simple_Thermostat.py
@@ -1,17 +1,19 @@
 from Ten_minute_temp import Ten_Minute_Average2
 from Energenie_Control import energenie_socket
 from Temperature_schedule import Target_temperature
+import RPi.GPIO as GPIO
 
 
-
-def Heat_or_no_heat():
+def Thermostat():
     Target = Target_temperature()
 
     if Ten_Minute_Average2() < Target: #Turn on socket
+        GPIO.cleanup()
         energenie_socket(1,True)
 
     elif Ten_Minute_Average2() >= Target: #Turn off socket
+        GPIO.cleanup()
         energenie_socket(1,False)
 
 
-Heat_or_no_heat()
+Thermostat()
